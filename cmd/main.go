bbba149e17679cb928af51d6dc7826e47aa63c30
GokulSNarayan <gokulnarayan09@gmail.com>
2024-02-24 23:02:22 +0530

    feat: added route handlers for crud operation

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"net/http"
 )
-
+var(
+	RecipeRe = regexp.MustCompile(`^/recipes/*$`)
+	RecipeReWithId = regexp.MustCompile(`^/recipes/([a-z0-9]+(?:-[a-z0-9]+)+)$`)
+)
 func main() {
 	mux := http.NewServeMux()
 
@@ -23,5 +26,29 @@ func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 type RecipesHandler struct {}
 
 func (h *RecipesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("This is my recipes page!"))
+switch {
+case r.Method == http.MethodPost && RecipeRe.MatchString(r.URL.PAth):
+	h.CreeateREcipe(w,r)
+	return
+case r.Method == http.MethodGet && RecipeRe.MatchString(r.URL.Path):
+	h.ListRecipes(w,r)
+	return
+case r.Method == http.MethodGet && RecipeRe.MatchString(r.URL.Path):
+	h.GetRecipe(w,r)
+	return
+case r.Method == http.MethodPut && RecipeRe.MatchString(r.URL.Path):
+	h.UpdateRecipe(w,r)
+	return
+	case r.Method == http.MethodDelete && RecipeRe.MatchString(r.URL.PAth):
+	h.DeleteRecipe(w,r)
+	return
+default:
+	return
 }
+
+func (h *RecipesHandler) CreateRecipes(w http.ResponseWriter, r *http.Request){}
+func (h *RecipesHandler) ListRecipes(w http.ResponseWriter, r *http.Request){}
+func (h *RecipesHandler) GetRecipes(w http.ResponseWriter, r *http.Request){}
+func (h *RecipesHandler) UpdateRecipes(w http.ResponseWriter, r *http.Request){}
+func (h *RecipesHandler) DeleteRecipes(w http.ResponseWriter, r *http.Request){}
+
